37fb493ab33f164426fe1812fc2c0548d65901fe
Jakub <jakublitwin.89@gmail.com>
2025-11-25 10:09:34 +0100

    Refactor main.h to include new headers

--- main.h
+++ main.h
@@ -1,22 +1,16 @@
-/*
- * main.h
- *
- *  Created on: Nov 23, 2025
- *      Author: darki
- */
-
 #ifndef MAIN_H_
 #define MAIN_H_
 
 #include <stdint.h>
 #include <stm32f3xx.h>
 #include "gpio.h"
-#include "Systick.h"
-#include "uart1.h"
-#include "uart2.h"
-#include "ubx.h"
+#include "SysTick.h"
+#include "uart.h"
+#include "gps.h"
+#include "i2c.h"
+#include "st7567s.h"
 
 
+#endif
 
 
-#endif /* MAIN_H_ */
