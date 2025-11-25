dba0f87d8950e58c50b6fe527d128cf1be107b96
Jakub <jakublitwin.89@gmail.com>
2025-11-25 10:16:49 +0100

    Update and rename ubx.h to gps.h

---
+++ gps.h
@@ -0,0 +1,30 @@
+#ifndef GPS_H
+#define GPS_H
+
+#include "stm32f3xx.h"
+#include <stdint.h>
+#include "SysTick.h"
+#include "uart.h"
+#include "st7567s.h"
+#include "i2c.h"
+
+
+extern float V;
+extern volatile uint8_t gps_line_ready;
+
+
+void GPS_Init(void);
+void UBX_SendCommand(const uint8_t* data, uint16_t len);
+
+
+void USART1_EXTI25_IRQHandler(void);
+void GPS_ProcessData(void);
+uint8_t GPS_IsLineReady(void);
+const char* GPS_GetLine(void);
+void GPS_ClearLine(void);
+
+
+void GpsSendToPC(void);
+void GpsDisplayLCD(void);
+
+#endif 
