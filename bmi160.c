bea95d84f827187ac72391caec9a6ebfbc4d179f
Jakub <jakublitwin.89@gmail.com>
2025-11-26 11:30:47 +0100

    Added interrupts for imu readings

--- bmi160.c
+++ bmi160.c
@@ -1,7 +1,8 @@
 #include "bmi160.h"
 
-#define BMI160_ADDR 0x69
 BMI160_Data bmi160_data;
+
+volatile uint8_t bmi160_data_ready = 0;
 
 void BMI160_Read(uint8_t reg_addr, uint8_t *data, uint8_t length)
 {
@@ -15,20 +16,37 @@ void BMI160_Write(uint8_t reg_addr, uint8_t data)
 
 void BMI160_Init(void)
 {
-	BMI160_Write(0x7E, 0x11); // accel normal mode
-	BMI160_Write(0x7E, 0x15); // gyro normal mode
-	delay_ms(1);
+	BMI160_Write(0x7E, 0xB6); //Soft reset
+	delay_ms(50);
 
-	// gyroscope
-	BMI160_Write(0x42, 0x28); // ODR 100Hz, normal filter
-	BMI160_Write(0x43, 0x00); // ±2000°/s
-	delay_ms(1);
+    BMI160_Write(0x7E, 0x11); //Accel Normal mode
+    delay_ms(4);
 
-	// accelerometer
-	BMI160_Write(0x40, 0x28); // ODR 100Hz, normal filter
-	BMI160_Write(0x41, 0x0C); // ±16g
-	delay_ms(1);
+    BMI160_Write(0x7E, 0x15); //Gyro Normal mode
+    delay_ms(80);
+
+    BMI160_Write(0x40, 0x28); //Accel ODR=100Hz
+    delay_us(500);
+
+    BMI160_Write(0x41, 0x0C); //Accel range=+-16g
+    delay_us(2);
+
+    BMI160_Write(0x42, 0x28); //Gyro ODR=100Hz
+    delay_us(2);
+
+	BMI160_Write(0x43, 0x00); //Gyro range =+-2000*
+	delay_us(2);
+
+	BMI160_Write(0x51, 0x10); // Turn on interrupts
+	delay_us(2);
+
+	BMI160_Write(0x56, 0x80); // Data ready to int1 pin
+	delay_us(2);
+
+	BMI160_Write(0x53, 0x0A); // int1 config: output enable, push-pull, active high
+	delay_us(2);
 }
+
 
 void BMI160_ReadData(BMI160_Data *d)
 {
@@ -45,4 +63,14 @@ void BMI160_ReadData(BMI160_Data *d)
 	d->ax = (int16_t)((buffer[1]<<8)|buffer[0]) * 16.0f * 9.80665f / 32768.0f;
 	d->ay = (int16_t)((buffer[3]<<8)|buffer[2]) * 16.0f * 9.80665f / 32768.0f;
 	d->az = (int16_t)((buffer[5]<<8)|buffer[4]) * 16.0f * 9.80665f / 32768.0f;
+}
+
+void EXTI1_IRQHandler(void)
+{
+    if (EXTI->PR & (1 << 1))
+    {
+        EXTI->PR |= (1 << 1);
+
+        bmi160_data_ready = 1;
+    }
 }
